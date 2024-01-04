df7d7542b27c3d0f5ba0d7f27155539955cb2b8d
Arun Motoori <arun.motoori@gmail.com>
2024-01-04 08:31:05 +0530

    Operators Covered

--- src/operators/OperatorsDemo.java
+++ src/operators/OperatorsDemo.java
@@ -4,13 +4,137 @@ public class OperatorsDemo {
 	
 	public static void main(String[] args) {
 		
+		int a = 's';
+		System.out.println(a);
+		
+		char b = 's';
+		System.out.println(b);
+		
+		char c = 115;
+		System.out.println(c);
+		
+		/*
+		int a = 5, b = 4, c = 3;
+		int d,e;
+		
+		d = a>b ? 11 : 22;
+		System.out.println(d);
+		
+		e = b<c ? 9 : 6;
+		System.out.println(e);
+		
+		
+		int i = 9, j = 4;
+		char h;
+		
+		h = i>j ? 's' : 'a';
+		
+		System.out.println(h);
+		
+		*/
+		
+		/*
+		int a = 5, b = 4;
+		
+		a+=b;   // a = a + b 
+		System.out.println(a);
+		
+		int c = 5, d = 4;
+		c-=d; // c = c-d
+		System.out.println(c);
+		
+		int e = 5, f = 4;
+		e*=f; // e = e * f
+		System.out.println(e);
+		
+		int g = 5,h = 3;
+		g/=h; // g = g/h;
+		System.out.println(g);
+		
+		int i = 5, j = 3;
+		i%=j; // i = i % j;
+		System.out.println(i);
+		*/
+		
+		/*
+		int a = 5; // Declaration and Initialization
+		System.out.println(a);
+		
+		int b;  // Declaration
+		b = 4;  // Initialization
+		System.out.println(b);
+		
+		int c = 9; // Declaration and Initialization
+		c = 3; // Re-initialization or Updating
+		System.out.println(c);
+		
+		
+		int a = 5;
+		int e,d,c,b;
+		
+		e = d = c = b = a;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		
+		*/
+		
+		
+		/*
+		boolean a = true, b = false, c = true, d = false;
+		
+		System.out.println(a&&b); //false
+		System.out.println(b&&c); //false
+		System.out.println(a&&c); //true
+		System.out.println(b&&d); //false
+		
+		System.out.println(a||b); //true
+		System.out.println(b||c); //true
+		System.out.println(a||c); //true
+		System.out.println(b||d); //false
+		
+		System.out.println(!a); //false
+		System.out.println(!b); //true
+		*/
+		
+		/*
+		int a = 9, b = 7, c = 5, d = 5, e = 3, f = 1;
+		
+		System.out.println(a==b); //false
+		System.out.println(c==d); //true
+		System.out.println(e==a); //false
+		
+		System.out.println(a!=b); //true
+		System.out.println(c!=d); //false
+		System.out.println(e!=a); //true
+		
+		System.out.println(a>b);  //true
+		System.out.println(c>d);  //false
+		System.out.println(e>a);  //false
+		
+		System.out.println(a<b);  //false
+		System.out.println(c<d);  //false
+		System.out.println(e<a);  //true
+		 
+		System.out.println(a>=b); //true
+		System.out.println(c>=d); //true
+		System.out.println(e>=a); //false
+		
+		System.out.println(a<=b); //false
+		System.out.println(c<=d); //true
+		System.out.println(e<=a); //true
+		*/
+		
+		
 		/*
 		System.out.println(5+4); // 9
 		System.out.println(5-4); // 1
 		System.out.println(5*4); // 20
 		System.out.println(5/3); // 1
 		System.out.println(5%3); // 2
-		*/
+		
 		
 		int a = 5;
 		System.out.println(a++);  // 5
@@ -24,6 +148,8 @@ public class OperatorsDemo {
 		int d = 5;
 		System.out.println(--d); //4
 		System.out.println(d); //4
+		
+		*/
 		
 	}
 
